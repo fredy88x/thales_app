92c38ccfbd4c3fad9f48d04f1ecd0d0b056bfbdf
fredy <fredy88x@yahoo.es>
2025-03-03 23:10:28 -0500

    add employee search component

--- frontend/src/app/components/sidebar.tsx
+++ frontend/src/app/components/sidebar.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
-import { Nav } from "react-bootstrap";
-import { FaUser } from "react-icons/fa";
 import Image from "next/image";
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar">
+    <aside className="w-64 bg-gray-900 text-white p-4">
       <Image
         className="styles.logo"
         src="/next.svg"
@@ -14,13 +12,15 @@ export default function Sidebar() {
         height={38}
         priority
       />
-      <h2 className="text-lg font-bold mb-4">Menú</h2>
-      <Nav className="flex-column">
-        <Link href="/" className="nav-link" passHref>
-          <FaUser />
-          Employee
+      <h2 className="text-lg font-bold mb-3">Menú</h2>
+      <nav className="space-y-2">
+        <Link href="/" className="flex p-2 rounded-md bg-gray-700" passHref>
+          <span className="m-2">Employee</span>
         </Link>
-      </Nav>
+        <Link href="/other" className="flex p-2 rounded-md bg-gray-700" passHref>
+          <span className="m-2">Other</span>
+        </Link>
+      </nav>
     </aside>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,98 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+interface Employee {
+  id: number;
+  employee_name: string;
+  employee_salary: number;
+  employee_age: number;
+  profile_image: string;
+}
 
 export default function Home() {
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const[employee, setEmployee] = useState<Employee | null>(null);
+  let hasContent: boolean = true;
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await axios
+      .get("http://dummy.restapiexample.com/api/v1/employee/1")
+      .then((response) => {
+        console.log(response.data);
+        setEmployee(response.data);
+      })
+      .catch((error) => {
+        console.error("Error getting employee:", error);
+      });
+
+     if (employee === null) {
+       await axios
+         .get("http://dummy.restapiexample.com/api/v1/employees")
+         .then((response) => {
+           console.log(response.data);
+           setEmployees(response.data);
+         })
+         .catch((error) => {
+           console.error("Error getting employees:", error);
+         });
+     }else{
+      hasContent = false;
+     } 
+
+  };
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-       
-      </main>
-    </div>
+    <main className="container">
+      <div className="card">
+        <div className="card-body">
+          <form
+            onSubmit={handleSubmit}
+            className="row justify-content-md-center"
+          >
+            <div className="col-auto">
+              <label htmlFor="employeeId" className="col-form-label">
+                Employee Id:
+              </label>
+            </div>
+            <div className="col-auto">
+              <input type="text" id="employeeId" className="form-control" />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Search
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="card mt-4" hidden={hasContent}>
+        <div className="card-body">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Salary</th>
+                <th scope="col">Age</th>
+                <th scope="col">Profile</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.map((employee) => (
+                <tr key={employee.id}>
+                  <th scope="row">{employee.id}</th>
+                  <td>{employee.employee_name}</td>
+                  <td>{employee.employee_salary}</td>
+                  <td>{employee.employee_age}</td>
+                  <td>{employee.profile_image}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
   );
 }
